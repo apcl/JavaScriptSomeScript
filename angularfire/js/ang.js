e21f9d490473b21d72d715065821eef65f5bdc84
Alejandro Cofré <alejandropcl@gmail.com>
2016-10-29 00:43:13 -0300

    Obtiene los datos con servicio firebaseArray

--- angularfire/js/ang.js
+++ angularfire/js/ang.js
@@ -1,14 +1,15 @@
 var app = angular.module("angfireMdl", ["firebase"]);
 app.controller("angfireCtrl", function($scope, $firebaseArray) {
-  var ref = firebase.database().ref().child("messages");
+  var ref = firebase.database().ref().child("Productos");
   // create a synchronized array
-  $scope.messages = $firebaseArray(ref);
+  $scope.productos = $firebaseArray(ref);
+  // console.log($scope.productos);
   // add new items to the array
   // the message is automatically added to our Firebase database!
-  $scope.addMessage = function() {
-    $scope.messages.$add({
-      text: $scope.newMessageText
-    });
-  };
+  // $scope.addMessage = function() {
+  //   $scope.messages.$add({
+  //     text: $scope.newMessageText
+  //   });
+  // };
   // click on `index.html` above to see $remove() and $save() in action
 });
